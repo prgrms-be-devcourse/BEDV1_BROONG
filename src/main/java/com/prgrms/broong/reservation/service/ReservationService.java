7a592162109b675f6e97dedbbc9d50099985bcea
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-31 03:19:44 +0900

    fix: Reservation Service 코드 수정#20

--- src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
+++ src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
@@ -1,22 +1,26 @@
 package com.prgrms.broong.reservation.service;
 
-import com.prgrms.broong.reservation.dto.CarReservationCheckDto;
+import com.prgrms.broong.reservation.domain.ReservationStatus;
 import com.prgrms.broong.reservation.dto.ReservationRequestDto;
 import com.prgrms.broong.reservation.dto.ReservationResponseDto;
 import com.prgrms.broong.user.dto.UserReservationCheckDto;
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 
 public interface ReservationService {
 
-    Long saveReservation(ReservationRequestDto reservationRequestDto);
+    Long saveReservation(ReservationRequestDto addReservationRequest,
+        UserReservationCheckDto userReservationCheckDto);
 
     ReservationResponseDto getReservation(Long reservationId);
 
     Page<ReservationResponseDto> getReservationListByUserId(Long userId);
 
-    boolean checkReservationByUserId(UserReservationCheckDto userReservationCheckDto);
+    boolean checkReservationByUserId(UserReservationCheckDto userReservationCheckDto,
+        ReservationStatus reservationStatus);
 
-    boolean possibleReservationTimeByCarId(CarReservationCheckDto carReservationCheckDto);
+    boolean possibleReservationTimeByCarId(Long carId, LocalDateTime checkTime,
+        ReservationStatus reservationStatus);
 
     Long removeReservation(Long reservationId);
 
