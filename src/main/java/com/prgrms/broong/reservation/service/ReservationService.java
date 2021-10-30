5f5622d5756cf182ea0207f308f94e45a0a94850
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-30 16:53:04 +0900

    feat: Reservation Service 조회 기능 추가

--- src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
+++ src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
@@ -1,12 +1,23 @@
 package com.prgrms.broong.reservation.service;
 
+import com.prgrms.broong.reservation.dto.CarReservationCheckDto;
 import com.prgrms.broong.reservation.dto.ReservationRequestDto;
 import com.prgrms.broong.reservation.dto.ReservationResponseDto;
+import com.prgrms.broong.user.dto.UserReservationCheckDto;
+import org.springframework.data.domain.Page;
 
 public interface ReservationService {
 
     Long saveReservation(ReservationRequestDto reservationRequestDto);
 
     ReservationResponseDto getReservation(Long reservationId);
+
+    Page<ReservationResponseDto> getReservationListByUserId(Long userId);
+
+    boolean checkReservationByUserId(UserReservationCheckDto userReservationCheckDto);
+
+    boolean possibleReservationTimeByCarId(CarReservationCheckDto carReservationCheckDto);
+
+    Long removeReservation(Long reservationId);
 
 }
