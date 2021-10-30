f566e1c2dcc8f8d61846c8f63574a881fd301fdf
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-30 16:54:15 +0900

    feat: Reservation Service 구현체 추가 기능 구현

--- src/main/java/com/prgrms/broong/reservation/service/ReservationServiceImpl.java
+++ src/main/java/com/prgrms/broong/reservation/service/ReservationServiceImpl.java
@@ -2,15 +2,21 @@ package com.prgrms.broong.reservation.service;
 
 import com.prgrms.broong.reservation.converter.ReservationConverter;
 import com.prgrms.broong.reservation.domain.Reservation;
+import com.prgrms.broong.reservation.domain.ReservationStatus;
+import com.prgrms.broong.reservation.dto.CarReservationCheckDto;
 import com.prgrms.broong.reservation.dto.ReservationRequestDto;
 import com.prgrms.broong.reservation.dto.ReservationResponseDto;
 import com.prgrms.broong.reservation.repository.ReservationRepository;
+import com.prgrms.broong.user.dto.UserReservationCheckDto;
 import java.text.MessageFormat;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class ReservationServiceImpl implements ReservationService {
 
@@ -25,7 +31,6 @@ public class ReservationServiceImpl implements ReservationService {
         return repository.save(reservation).getId();
     }
 
-    @Transactional(readOnly = true)
     @Override
     public ReservationResponseDto getReservation(Long reservationId) {
         return repository.findById(reservationId)
@@ -34,4 +39,35 @@ public class ReservationServiceImpl implements ReservationService {
                 MessageFormat.format("해당 {0}키의 객체를 찾을 수 없습니다.", reservationId)));
     }
 
+    @Override
+    public Page<ReservationResponseDto> getReservationListByUserId(Long userId) {
+        return repository.findReservationsByUserId(userId)
+            .map(converter::ReservationToResponseDto);
+    }
+
+    @Override
+    public boolean checkReservationByUserId(UserReservationCheckDto userReservationCheckDto) {
+        Optional<Reservation> checkReservation =
+            repository.checkReservationByUserId(userReservationCheckDto.getId(),
+                userReservationCheckDto.getCheckTime());
+        return checkReservation.isPresent();
+    }
+
+    @Override
+    public boolean possibleReservationTimeByCarId(CarReservationCheckDto carReservationCheckDto) {
+        Optional<Reservation> checkReservation =
+            repository.possibleReservationTimeByCarId(carReservationCheckDto.getCarId(),
+                carReservationCheckDto.getCheckTime());
+        return checkReservation.isPresent();
+    }
+
+    @Transactional
+    @Override
+    public Long removeReservation(Long reservationId) {
+        Reservation reservation = repository.findById(reservationId)
+            .orElseThrow(() -> new RuntimeException(
+                MessageFormat.format("해당 {0}키의 객체를 찾을 수 없습니다.", reservationId)));
+        reservation.changeReservationStatus(ReservationStatus.CANCELD);
+        return reservationId;
+    }
 }
