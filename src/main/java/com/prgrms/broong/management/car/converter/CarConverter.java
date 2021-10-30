cf90e696acd8397560e495e86575682e083c8822
venzym <venzym@naver.com>
2021-10-30 17:13:31 +0900

    feat: carConverter 추가 #6

--- src/main/java/com/prgrms/broong/management/car/converter/CarConverter.java
+++ src/main/java/com/prgrms/broong/management/car/converter/CarConverter.java
@@ -1,2 +1,34 @@
-package com.prgrms.broong.management.car.converter;public class CarConverter {
+package com.prgrms.broong.management.car.converter;
+
+import com.prgrms.broong.management.car.domain.Car;
+import com.prgrms.broong.management.car.dto.CarRequestDto;
+import com.prgrms.broong.management.car.dto.CarResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarConverter {
+
+    public Car carToEntity(CarRequestDto carRequestDto) {
+        return Car.builder()
+            .carNum(carRequestDto.getCarNum())
+            .model(carRequestDto.getModel())
+            .fuel(carRequestDto.getFuel())
+            .price(carRequestDto.getPrice())
+            .possiblePassengers(carRequestDto.getPossiblePassengers())
+            .species(carRequestDto.getSpecies())
+            .build();
+    }
+
+    public CarResponseDto carToResponseDto(Car car) {
+        return CarResponseDto.builder()
+            .id(car.getId())
+            .carNum(car.getCarNum())
+            .model(car.getModel())
+            .model(car.getModel())
+            .fuel(car.getFuel())
+            .possiblePassengers(car.getPossiblePassengers())
+            .species(car.getSpecies())
+            .build();
+    }
+
 }
