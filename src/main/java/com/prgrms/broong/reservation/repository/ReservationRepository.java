96d004ccd00f0c1d19e1566940314062de9ec23f
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-11-01 01:14:27 +0900

    fix: Reservation Repository 코드 수정#20

--- src/main/java/com/prgrms/broong/reservation/repository/ReservationRepository.java
+++ src/main/java/com/prgrms/broong/reservation/repository/ReservationRepository.java
@@ -5,6 +5,7 @@ import com.prgrms.broong.reservation.domain.ReservationStatus;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,11 +15,11 @@ import org.springframework.stereotype.Repository;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     // userId 와 reservationId로 엮인 쿼리
-    Page<Reservation> findReservationsByUserId(Long userId);
+    Page<Reservation> findReservationsByUserId(Long userId, Pageable pageable);
 
     // userId , 예약시간 으로 이미 예약된게 있는지 확인하는 쿼리
     @Query(value = "SELECT r FROM Reservation r WHERE r.user.id = :userId"
-        + " AND r.reservationStatus = :status"
+        + " AND r.reservationStatus <> :status"
         + " AND r.startTime <= :checkTime"
         + " AND r.endTime >= :checkTime")
     Optional<Reservation> checkReservationByUserId(@Param("userId") Long userId,
@@ -26,13 +27,13 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     // 예약시간 + carId 로 해당 시간에 예약이 가능한지 확인하는 쿼리
     @Query(value = "SELECT r FROM Reservation r WHERE r.parkCar.car.id = :carId"
-        + " AND r.reservationStatus = :status"
+        + " AND r.reservationStatus <> :status"
         + " AND r.startTime <= :checkTime"
         + " AND r.endTime >= :checkTime")
     Optional<Reservation> possibleReservationTimeByCarId(@Param("carId") Long carId,
         @Param("checkTime") LocalDateTime checkTime, @Param("status") ReservationStatus status);
 
-    @Query(value = "SELECT r FROM Reservation r JOIN FETCH User u WHERE r.id = :reservationId")
+    @Query(value = "SELECT r FROM Reservation r JOIN FETCH r.user WHERE r.id = :reservationId")
     Optional<Reservation> findReservationAndUser(@Param("reservationId") Long reservationId);
 
 }
