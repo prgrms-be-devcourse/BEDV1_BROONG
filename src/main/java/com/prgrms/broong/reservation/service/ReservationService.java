df9d5ccc323fe96c71339fcbd8b2e84405c00d6f
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-11-02 22:54:20 +0900

    fix: ReservationService 수정 및 코드 추가#31

--- src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
+++ src/main/java/com/prgrms/broong/reservation/service/ReservationService.java
@@ -1,12 +1,28 @@
 package com.prgrms.broong.reservation.service;
 
+import com.prgrms.broong.reservation.domain.ReservationStatus;
 import com.prgrms.broong.reservation.dto.ReservationRequestDto;
 import com.prgrms.broong.reservation.dto.ReservationResponseDto;
+import com.prgrms.broong.user.dto.UserReservationCheckDto;
+import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReservationService {
 
-    Long saveReservation(ReservationRequestDto reservationRequestDto);
+    Long saveReservation(ReservationRequestDto addReservationRequest,
+        UserReservationCheckDto userReservationCheckDto);
 
     ReservationResponseDto getReservation(Long reservationId);
+
+    Page<ReservationResponseDto> getReservationListByUserId(Long userId, Pageable pageable);
+
+    boolean checkReservationByUserId(UserReservationCheckDto userReservationCheckDto,
+        ReservationStatus reservationStatus);
+
+    boolean possibleReservationTimeByCarId(Long carId, LocalDateTime checkTime,
+        ReservationStatus reservationStatus);
+
+    Long removeReservation(Long reservationId);
 
 }
