547ecf76642e864ee0a93cfad1d86450ca2f4335
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-11-02 23:12:04 +0900

    feat: 여러 Converter 생성 및 변환메서드 추가#31

---
+++ src/main/java/com/prgrms/broong/management/domain/converter/ParkCarConverter.java
@@ -0,0 +1,34 @@
+package com.prgrms.broong.management.domain.converter;
+
+import com.prgrms.broong.management.car.converter.CarConverter;
+import com.prgrms.broong.management.domain.ParkCar;
+import com.prgrms.broong.management.dto.ParkCarResponseDto;
+import com.prgrms.broong.management.park.converter.ParkConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParkCarConverter {
+
+    @Autowired
+    private ParkConverter parkConverter;
+
+    @Autowired
+    private CarConverter carConverter;
+
+    public ParkCar parkCarResponseToEntity(ParkCarResponseDto parkCarResponseDto) {
+        return ParkCar.builder()
+            .id(parkCarResponseDto.getId())
+            .park(parkConverter.parkResponseToEntity(parkCarResponseDto.getParkResponseDto()))
+            .car(carConverter.carResponseDtoToEntity(parkCarResponseDto.getCarResponseDto()))
+            .build();
+    }
+
+    public ParkCarResponseDto ParkToResponseDto(ParkCar parkCar) {
+        return ParkCarResponseDto.builder()
+            .id(parkCar.getId())
+            .parkResponseDto(parkConverter.parkToResponseDto(parkCar.getPark()))
+            .carResponseDto(carConverter.carToResponseDto(parkCar.getCar()))
+            .build();
+    }
+}
