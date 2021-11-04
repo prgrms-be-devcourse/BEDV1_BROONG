2b9600c241f24f99878f36e74a845321b8717910
pinoa1228 <pinoa1228@gmail.com>
2021-11-05 02:40:34 +0900

    fix: 멘토님 피드백 반영(테이블 이름 수정) #55

--- src/main/java/com/prgrms/broong/user/domain/User.java
+++ src/main/java/com/prgrms/broong/user/domain/User.java
@@ -26,7 +26,7 @@ import org.hibernate.validator.constraints.Length;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User extends BaseEntity {
 
     @Id
