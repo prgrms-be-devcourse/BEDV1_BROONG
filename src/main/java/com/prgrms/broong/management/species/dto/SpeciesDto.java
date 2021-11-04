04147fb2bbe39d37b193e4f9d3e33a225e577346
venzym <venzym@naver.com>
2021-11-04 16:25:11 +0900

    fix: 컨트롤러 동작을 위한 생성자 어노테이션 추가 #49

--- src/main/java/com/prgrms/broong/management/species/dto/SpeciesDto.java
+++ src/main/java/com/prgrms/broong/management/species/dto/SpeciesDto.java
@@ -1,14 +1,21 @@
 package com.prgrms.broong.management.species.dto;
 
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SpeciesDto {
 
+    @NotNull
     private Long id;
 
+    @NotNull
     private String name;
 
 }
