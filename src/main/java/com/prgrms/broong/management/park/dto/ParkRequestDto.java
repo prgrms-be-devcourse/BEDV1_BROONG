60e84e79af0d8bb8ebd166d58021cb70cf8e68d5
venzym <venzym@naver.com>
2021-11-04 16:13:35 +0900

    feat: 컨트롤러 동작을 위한 생성자 어노테이션 추가 #45

--- src/main/java/com/prgrms/broong/management/park/dto/ParkRequestDto.java
+++ src/main/java/com/prgrms/broong/management/park/dto/ParkRequestDto.java
@@ -1,12 +1,20 @@
 package com.prgrms.broong.management.park.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParkRequestDto {
 
+    @NotNull(message = "null값 입니다.")
+    @Min(message = "0이상이여야 합니다", value = 0)
     private Integer possibleNum;
 
     private LocationDto locationDto;
