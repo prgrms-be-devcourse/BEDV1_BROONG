3b3a67938930c6b325ed5791379ffa4c615398e3
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-11-05 14:42:24 +0900

    fix: Reservation Domain 코드 수정#57

--- src/main/java/com/prgrms/broong/reservation/domain/ReservationStatus.java
+++ src/main/java/com/prgrms/broong/reservation/domain/ReservationStatus.java
@@ -2,9 +2,9 @@ package com.prgrms.broong.reservation.domain;
 
 public enum ReservationStatus {
 
-    RESERVATION,
-    USING,
-    RETURN,
-    CANCELD
+    READY,
+    PROCEED,
+    COMPLETE,
+    CANCEL
 
 }
